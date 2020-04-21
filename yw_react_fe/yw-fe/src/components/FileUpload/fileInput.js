8004b5d3631540c578e792c61e85bae1ddf8f319
Mike <mike@Helens-iMac.local>
2020-04-21 18:21:49 +0100

    Added in validation around file type and tests

--- yw_react_fe/yw-fe/src/components/FileUpload/fileInput.js
+++ yw_react_fe/yw-fe/src/components/FileUpload/fileInput.js
@@ -6,17 +6,32 @@ import './fileUpload.css'
 const FileInput = () => {
 
   const ref = React.useRef(null);
+  const [label , setLabel] = React.useState('Choose a csv file…');
+
 
   const HiddenInput = () => (
-    <input ref={ref} type="file" name="resume" style={{display: 'none'}}/>
+    <input accept=".csv, text/csv, application/csv"
+           onChange={(e) => {
+             //ToDo More robust error handling in case
+             // someone decides to bypass the accept.
+             if(e.currentTarget.files[0].type.indexOf('csv') !== -1) {
+               setLabel(e.currentTarget.files[0].name)
+             }
+          } }
+           data-testid="hidden-input"
+           ref={ref}
+           type="file"
+           name="resume"
+           style={{display: 'none'}}
+    />
   )
   return (
     <div data-testid="component-file-upload" className="file" onClick={() => ref.current.click()}>
      <HiddenInput />
       <label className="file-label">
           <span className="file-cta">
-            <span className="file-label">
-            Choose a file…
+            <span data-testid="fileLabel" className="file-label">
+            {label}
           </span>
             <span className="file-icon">
                <FontAwesomeIcon icon={faFileCsv} />
--- yw_react_fe/yw-fe/src/components/FileUpload/fileUpload.test.js
+++ yw_react_fe/yw-fe/src/components/FileUpload/fileUpload.test.js
@@ -5,29 +5,71 @@ import FileUpload from "./fileInput";
 
 
 describe('<FileUpload/>', () => {
+  let queryByTestId;
+  beforeEach(() => {
+    ({queryByTestId} = render(<FileUpload />));
+  })
   test('renders without error', () => {
-    const { queryByTestId } = render(<FileUpload />);
     const uploadElement = queryByTestId('component-file-upload')
     expect(uploadElement).toBeInTheDocument();
   });
 
-  test('when clicked on opens a file dialog', () => {
-    /*
-      Mock out the ref object as that what gets called when
-      its added to the ref.
-     */
-   let mockRef = jest.spyOn(React, 'useRef').mockReturnValue({
-     current: {
-       click: jest.fn()
-     }
-   })
-    /*
-      Get the upload element and check that the mocked Event has been called.
-     */
-    const { queryByTestId, getByRole } = render(<FileUpload />);
-    const uploadElement = queryByTestId('component-file-upload')
-    userEvent.click(uploadElement);
-    expect(mockRef).toHaveBeenCalled();
-
+  test('Has a label with default text', () => {
+    const defaultText = "Choose a csv file…";
+    const fileLabelElement = queryByTestId('fileLabel')
+    expect(fileLabelElement.textContent).toContain(defaultText);
   });
+
+  describe('When Clicked', () => {
+    let mockRef = jest.spyOn(React, 'useRef').mockReturnValue({
+      current: {
+        click: jest.fn()
+      }
+    })
+    test('opens a file dialog', () => {
+      const uploadElement = queryByTestId('component-file-upload')
+      userEvent.click(uploadElement);
+      expect(mockRef).toHaveBeenCalled();
+
+    });
+  });
+
+  describe('When file selected', () => {
+    let hiddenFileInputElement;
+    let mockEvent;
+    const fireFileUploadEvent = (fileName, fileType) => {
+      /*
+        I've decided to mock out the event , I could have just as
+        easily spied on React.useState and set the value of the
+        file name.
+       */
+      const testFile = new Blob(
+        [{fileContents: 'tesying'}], {type: fileType});
+      testFile.name = fileName
+      mockEvent = {target: {files: [testFile]}};
+      fireEvent.change(hiddenFileInputElement, mockEvent);
+    }
+    beforeEach(() => {
+      hiddenFileInputElement = queryByTestId('hidden-input');
+    })
+    test('it displays the filename selected', () => {
+      const testFileName = 'my-test-file.csv';
+      const testFileType = 'application/csv';
+      fireFileUploadEvent(testFileName, testFileType);
+
+      const labelText = queryByTestId('fileLabel');
+      expect(labelText.textContent).toContain(testFileName);
+    });
+
+    test('it can only accept csv', () => {
+      const defaultText = "Choose a csv file…";
+      const testFileName = 'my-test-file.js';
+      const testFileType = 'application/json';
+
+      fireFileUploadEvent(testFileName, testFileType);
+
+      const labelText = queryByTestId('fileLabel');
+      expect(labelText.textContent).toContain(defaultText);
+    });
+  })
 })
